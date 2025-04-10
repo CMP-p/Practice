b49e5529c0aa86131f16cd44deef07bf45f9b337
CMP-p <205021359+CMP-p@users.noreply.github.com>
2025-04-10 06:15:44 -0700

    changed file contents, copied to repo, whats it show?

--- VscodePy.py
+++ VscodePy.py
@@ -1,19 +1,18 @@
 #All of the commands i know, print(), if, for,
 #while, else, elif, def():, range(), input(), str()
-#int(), bool(), float(), True, False, variable = val
+#int(), bool(), float(), True, False, avariable = val
 #<,<=,>,>=,==, is, is not, +, -, *, **, %, #, 
-#var.capitalize, var.title, var.upper, var.lower,
-#set(), list(), dict(), dict={key:val}, list = [val]
-#set = {val}, tuple = val1, val2... or atuple = ()
-#tuple(), print('txt \n' 'newline'), list2 = [0:2],
-#newlist = alist[2:4], type(), var = list[0], 
-#vars = set or vars = list, newlist=[-4:-3] start stop step
+#str.capitalize, str.title, str.upper, str.lower,
+#set(), list(), dict(), dict={key1:val1, key2:val2}, list = [val1, val2...]
+#set = {val1, val2...}, atuple = val1, val2... or Empty_tuple = ()
+#tuple(), print('txt \n' 'newline'), newlist = alist[2:4], type(), 
+# var = list[0], vars = set or vars = list, newlist=[-4:-3,1] start stop step
 #len(), a_str.find(), list.append(), list.inset(dex,val)
 
 vari = input()
 #Random function play
 def hey():
-    for i in range(100):
+    for i in range(len(vari)):
         print('hey' + 'y'*i)
     print ("NO"+'o'*len(vari))
 #calling said function
@@ -28,16 +27,22 @@ print(input('well, what about ' + str(vari) + '?\n')+ '???', '\nokay, well solve
 #question to pass the first gate
 test2 = 3**4
 answer2 = int(input('What is 3^4?:'))
+ur_list=[]
 
 
 Gated = True
-while Gated == True:
+while Gated:
     if answer2 == test2:
         print ('Well done! Pass!')
         Gated = False
     else: 
         print(test2)
         answer2 = int(input('NOPE! trry again:\n'))
-Gated = False
-print('\n\n\n\n\n\n')
-print('Welcome to they new Palace!')
+        ur_list.append(answer2)
+
+#testing slicing and list appendages
+print("Here's all yout wrong answers! ;) \n", ur_list)
+list_alt = ur_list[0:2]
+print (list_alt, '\ndone')
+
+print('Welcome to the Python Palace! :)')
