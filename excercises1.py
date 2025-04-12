d65d8d33cd39c4b3f87e2143dd4a56898bd74ae9
CMP-p <205021359+CMP-p@users.noreply.github.com>
2025-04-12 08:12:35 -0700

    completed grosspay excercise. integrated first ecercise

--- excercises1.py
+++ excercises1.py
@@ -1,8 +1,15 @@
 # Greet user by name
+
 def greet():
     name = input("What's your name? \n").title()
     print(f'Wuz good {name}?')
+    return name
 
-greet()
-quit()
+name = greet()
+#quit()
 
+
+# Gross pay Calculator
+Hours = float(input('How many hours did you work? \n'))
+Wage = float(input("What's your hourly rate? \n"))
+print("You're gross pay is" , '$' + str(Wage*Hours) + '0,', name)
