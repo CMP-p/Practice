b0cc8219c22fa343180fee6f0e2dcc14e92b2375
CMP-p <205021359+CMP-p@users.noreply.github.com>
2025-04-13 08:54:41 -0700

    Rewriting calculator to give 1.5 times the pay for overtime, with input capture

--- excercises1.py
+++ excercises1.py
@@ -10,6 +10,37 @@ name = greet()
 
 
 # Gross pay Calculator
+'''''
 Hours = float(input('How many hours did you work? \n'))
 Wage = float(input("What's your hourly rate? \n"))
 print("You're gross pay is" , '$' + str(Wage*Hours) + '0,', name)
+'''
+
+# Rewriting calculator to give 1.5 times the pay for overtime, with input capture
+check = True #to exit while loops
+
+while check is True:
+    try: 
+        Hours = float(input('How many hours did you work? \n'))
+        check = False
+    except:
+        print('Please enter a valid number. \n')
+check = True #resetting exit condition
+
+while check is True:
+    try:
+        Wage = float(input('Whats your hourly rate? \n'))
+        check = False
+    except: 
+        print(f"Please enter a Valid number.")
+check = True
+
+pay = False #defining variable globally before used
+if Hours > 40:
+    pay = ((Hours-40)*Wage*1.5)+(40*Wage)
+    print('Overtime earned.')
+else: pay = Wage*Hours
+
+
+print('$'+str(pay)+'0', 'should be on the check buddy.') 
+
