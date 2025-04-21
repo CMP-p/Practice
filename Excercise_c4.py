5eb6dc0844016cea21120dc4eae5dd431e9970a7
CMP-p <205021359+CMP-p@users.noreply.github.com>
2025-04-21 03:35:08 -0700

    to be honest I dont know what excercise Im on, lets call it 8

--- Excercise_c4.py
+++ Excercise_c4.py
@@ -20,4 +20,13 @@ while True:
 average = total/count
 print(f"Your Average is: {average}\nYour Count is:{count},\nYour Total is {total}") 
     
-    
+
+# Excercises 8? idk I've lost count
+# Locating and converting a float in a known string
+prompt_str = 'X-DSPAM-Confidence: 0.8475 '
+# nums = float(prompt_str[prompt_str.find(': ') +1:].strip()) # This version is hard to read, but does the same as below
+col_loc = prompt_str.find(': ')
+new_str = prompt_str[col_loc + 1:]#slicing to the end of the string
+nums = new_str.strip() #removing whitespaces
+nums = float(nums) #converting to float
+print(nums, type(nums)) 
