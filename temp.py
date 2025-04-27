290a46c2eacb30e89abc66fb2312622eb8322b99
CMP-p <205021359+CMP-p@users.noreply.github.com>
2025-04-27 08:37:14 -0700

    I made a temp program, it'll be overwritten in my next push, but this program evaluates names lexicographically, and then assigns the name to it's lexicographic value in a dictionary.

--- temp.py
+++ temp.py
@@ -1,33 +1,15 @@
 #I run examples in here
-#make a program that takes input text, counts the words, and outputs the word with the most repitions
-
-line = input('enter text here: \n')
-words = line.split()
-print('\n'*4)
-counter = {}
-for i in words:
-    counter[i] = counter.get(i,0) + 1
-
-key_max = None
-value_max = None
-for i,j in counter.items():
-    if value_max is None or value_max < j:
-        key_max, value_max = i,j
-        
+def calcul(inp):
+    your_str = inp
+    num = 0
+    for i in your_str:
+        num += ord(i)
+    return num
 
 
-print(key_max,value_max)
+names = ['Christian', "Kayla", "Jeremy"]
+val_name = {}
+for name in names:
+    val_name[name] = val_name.get(name, calcul(name))
 
-
-
-
-
-'''
-Alternate for dict.get(x,y)
-for i in heya:
-    if i not in heyo:
-        heyo[i] = 1
-    else:
-        heyo[i] += 1
-print (heyo)
-'''    
+print(val_name)
