346164189bdb5491af19985296e566a55a690e9d
CMP-p <205021359+CMP-p@users.noreply.github.com>
2025-05-02 04:00:44 -0700

    temp.py got an update using regex and dictionary creation. and misc.

--- temp.py
+++ temp.py
@@ -1,18 +1,32 @@
 #I practice in here with the lesson
-
 import re
+fhandle = open('mbox-short.txt')
+conf_list = []
+pat = '^X.+C.+: ([0-9.]+)'
+counter = 0
+for line in fhandle:
+    counter += 1
+    if counter == -1: break
+    finds = re.findall(pat, line)
+    if len(finds) != 1: continue
+    if float(finds[0]) >= 0:
+        conf_list.append(finds[0])
 
-stwing1 = 'this is test one'
-stwing2 = 'this is test two'
-stwing3 = 'this is test three'
-stwings = stwing1,stwing2,stwing3
-found = list()
-for i in stwings:
-    if re.search(r't.+\s+t.+\s+t', i):
-        found.append(re.findall(r'[set]+',i))
-        print(i)
-    else: print('hehe okay buddy\vI saw that')
+print(conf_list,len(conf_list),)
+fhandle.close()
 
-print(found)
 
-#Practicing with regex re.search(patter, text) and re.findall(pattern, text) 
+dict2pct = {}
+copies = 0
+
+for v in conf_list:
+    k = str(float(v[:5])*100)+'%'
+    if k in dict2pct:
+        copies += 1
+        k = k + f' C {copies}'
+    dict2pct[k] = v
+print (dict2pct, len(dict2pct), len(conf_list))
+
+# searching for the following line : X-DSPAM-Confidence: 0.875
+#makes a dictionary andcorrolating keys to a percent, and the number it was before. I didn't include rounding because 
+# Time is of the essenece,so I assigned a copy counter. xP this was allextra, not even an assignment
