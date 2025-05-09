222ea6bd993f0ecbc3caf06d8cbba054430c6034
CMP-p <205021359+CMP-p@users.noreply.github.com>
2025-05-09 04:29:09 -0700

    sockets, data, en/decoding, wtf is AF_INET

--- temp.py
+++ temp.py
@@ -1,32 +1,22 @@
 #I practice in here with the lesson
-import re
-fhandle = open('mbox-short.txt')
-conf_list = []
-pat = '^X.+C.+: ([0-9.]+)'
-counter = 0
-for line in fhandle:
-    counter += 1
-    if counter == -1: break
-    finds = re.findall(pat, line)
-    if len(finds) != 1: continue
-    if float(finds[0]) >= 0:
-        conf_list.append(finds[0])
+import socket
 
-print(conf_list,len(conf_list),)
-fhandle.close()
+mysock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+print(mysock)
+mysock.connect(('data.pr4e.org', 80))
+cmd = 'GET http://data.pr4e.org/romeo.txt HTTP/1.0\r\n\r\n'.encode()
+mysock.send(cmd)
 
+dati = b''
+while True:
+    data = mysock.recv(512)
+    if len(data) < 1:
+        break
+    print(data.decode())
+    dati += data
+mysock.close()
 
-dict2pct = {}
-copies = 0
+print('/n/n', dati.decode())
 
-for v in conf_list:
-    k = str(float(v[:5])*100)+'%'
-    if k in dict2pct:
-        copies += 1
-        k = k + f' C {copies}'
-    dict2pct[k] = v
-print (dict2pct, len(dict2pct), len(conf_list))
-
-# searching for the following line : X-DSPAM-Confidence: 0.875
-#makes a dictionary andcorrolating keys to a percent, and the number it was before. I didn't include rounding because 
-# Time is of the essenece,so I assigned a copy counter. xP this was allextra, not even an assignment
+# Pracrticing with TCP? something about sockets, ports and extensions
+# recieving byte strings, looping through data and decoding the results
