2ff20bafb9011de625cf6d020889ca0eaad6995c
CMP-p <205021359+CMP-p@users.noreply.github.com>
2025-06-18 08:34:39 -0700

    Doing some familiarity building, and finally continuing the lessosn. The PY4E networking section is highly convoluted.

--- temp.py
+++ temp.py
@@ -1,21 +1,46 @@
-#I practice in here with the lesson
-import urllib.error, urllib.parse, urllib.request
-from bs4 import BeautifulSoup
-
-url = input('Enter- ')
-html = urllib.request.urlopen(url).read()
-soup = BeautifulSoup(html, 'html.parser')
-
-tags = soup('a')
-for tag in tags:
-    print(tag.get('href', None))
-
-print('done')
-
-
-#importing urllib library modules and some dictionary refreshing
+import socket
+#importing the socket library
+Mysock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+#calling the socket library, then the socket function/class constructor. Then calling 2 special variables in the library
+Mysock.connect(('data.pr4e.org', 80))
+#connecting the socket to the website on port 80 which is typical for html ports does 
 
 '''
-I took a huge break. Felt a lot of overwhelm. I suppose I needed to straightne out some mental/emotional barriers.
-I'm well enough to continue, taking a more mindful approach.
+Does the RandomSocket.Conntect function, create the "line of communication" between the socket/port on my computer to
+the server that I've identified? Could I replace 'data.py4e.org' with the server addres? 
+Yes, appearently i can'''
+
+cmd = 'GET /romeo.txt HTTP/1.0\r\nHost: data.pr4e.org\r\n\r\n'.encode()
+Mysock.sendall(cmd)
+
+
 '''
+Okay, but how about .recieve()? if i send nothing, what will be recieved? what if I've recieved all, will noting more be
+recieved? What if i send something but never recieve. What if i send something invalid, and then something valid, and then 
+recieve, will I recieve both portions of data?'''
+'''
+You'll get a server error. This specific example, the serrver will close your connection and not listesn for new requests
+and depending on the error, will give you an error 400, bad request, or error 501, not implemented
+'''
+
+
+data2print = str()
+while True:
+    data = Mysock.recv(512).decode()
+    if len(data) < 1:
+        break
+    else: data2print += data
+    print(data2print)
+
+'''while True:
+    data = Mysock.recv(512).decode()
+    if len(data) > 1:
+        print(data)
+    else: break
+    (alternate code block)
+    '''
+Mysock.close()
+
+
+#I was doing some testing around, building comprehension on sockets. Even though they aren't used. I wanted to refound myself 
+#before moving further ahead. 
