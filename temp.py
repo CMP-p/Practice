7d81ee4e3e260290d869977fc3f96803b1bf4446
CMP-p <205021359+CMP-p@users.noreply.github.com>
2025-07-18 04:45:36 -0700

    I'm doing this backwards it would seem xD

--- temp.py
+++ temp.py
@@ -1,46 +1,20 @@
 ''''''
 import json
-import urllib.parse, urllib.error, urllib.request
 ''''''
+#Excercise c13_2
+data = '''
+{
+    "name" : "Chuck",
+    "phone" : {
+        "type" : "intl",
+        "number" : "+1 734 303 4456"
+    },
+    "email" : {
+        "hide" : "yes"
+    }    
+}'''
 
-apiurl = 'https://nominatim.openstreetmap.org/search?'
-failed_attempt = 0
+js = json.loads(data)
+print('Data',type(data),"\njs",type(js))
 
-while True: 
-    address = input('Enter location dawg: ')
-    if len(address) < 1: break
-    
-    #url enconding with query and params
-    url = apiurl + urllib.parse.urlencode({'format':'json',
-                                               'q':address
-                                               })
-
-    print('Retrieving', url)
-    urlHand = urllib.request.urlopen(url)
-    data = urlHand.read().decode()
-    js = json.loads(data) #converts inbound Json data to Python
-
-    if len(js) == False:
-        print('Error: Not a valid query')
-        failed_attempt += 1
-        if failed_attempt > 3: 
-            print('To many failed attempts, please try again later.')
-            break
-        continue
-    else: 
-        print(len(data), 'charachters to process')
-
-    #find lat, lang and location
-    print('Results: \n', json.dumps(js, indent=4))
-
-    lat, lon, loc = (float(js[0]['lat']),
-                     float(js[0]['lon']),
-                     js[0]['display_name'])
-    print ('lat:',lat,
-           'lon:', lon, 
-           'loc:', loc)
-    urlHand.close()
-    break
-
-
-
+print(js['name'],'\n',js['email']['hide'])
